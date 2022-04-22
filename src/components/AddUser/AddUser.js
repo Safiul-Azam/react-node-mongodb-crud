5b11c55dde657393f94d698b8b2cf2cbd9cebeaa
mdsafiulazam <mdriadkhan20@gmail.com>
2022-04-22 12:45:33 +0600

    created a form and data get and user POST method send req

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 const AddUser = () => {
+
+    const handleAddUser = e => {
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value 
+        const user = {name, email}
+        fetch('http://localhost:5000/user', {
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data=>{
+            console.log('success', data)
+            alert('successfully your email and name is went')
+            e.target.reset()
+        })
+    }
     return (
         <div>
-            <h3> this is home section</h3>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" placeholder='Name' />
+                <br />
+                <input type="email" name="email" placeholder='Email' />
+                <br />
+                <input type="submit" value="add " />
+            </form>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
             <h3>this is home section</h3>
+            <Link to='/add-user'>Add user</Link>
         </div>
     );
 };
